9bbbf8ae834d6ae9ffb9929a266724eb1372b9ab
Joao Henrique <joaohenrique.reis18@gmail.com>
2022-01-25 19:38:37 -0300

    Estado e cidade

--- src/main/java/com/jh/cursomc/domain/Produto.java
+++ src/main/java/com/jh/cursomc/domain/Produto.java
@@ -18,84 +18,79 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 public class Produto implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //Nessa annotation estou definindo uma estrategia de geração automatica do ID
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Nessa annotation estou definindo uma estrategia de geração
+														// automatica do ID
 	private Integer id;
 	private String nome;
 	private double preco;
-	
-  @JsonBackReference	
-  @ManyToMany // Essa annottation faz que tenha um relacionamento que duas entidades se relacionam entre si de forma múltipla.
-  @JoinTable(name = "PRODUTO_CATEGORIA",
-             joinColumns = @JoinColumn(name = "produto_id"),
-             inverseJoinColumns = @JoinColumn(name="categoria_id")) //
-  private List<Categoria>categorias = new ArrayList<>();	
-	
-  Produto(){
-	  
-  }
 
-public Produto(Integer id, String nome, double preco) {
-	super();
-	this.id = id;
-	this.nome = nome;
-	this.preco = preco;
-}
+	@JsonBackReference
+	@ManyToMany // Essa annottation faz que tenha um relacionamento que duas entidades se
+				// relacionam entre si de forma múltipla.
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id")) //
+	private List<Categoria> categorias = new ArrayList<>();
 
-public Integer getId() {
-	return id;
-}
+	Produto() {
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	}
 
-public String getNome() {
-	return nome;
-}
+	public Produto(Integer id, String nome, double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
 
-public void setNome(String nome) {
-	this.nome = nome;
-}
+	public Integer getId() {
+		return id;
+	}
 
-public double getPreco() {
-	return preco;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public void setPreco(double preco) {
-	this.preco = preco;
-}
+	public String getNome() {
+		return nome;
+	}
 
-public List<Categoria> getCategorias() {
-	return categorias;
-}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-public void setCategorias(List<Categoria> categorias) {
-	this.categorias = categorias;
-}
+	public double getPreco() {
+		return preco;
+	}
 
-@Override
-public int hashCode() {
-	return Objects.hash(id);
-}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Produto other = (Produto) obj;
-	return Objects.equals(id, other.id);
-}
-  
-  
-  
-	
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+
 }
